96de4c924991b802c70cf12afba4ddeecd44d90e
MartL7 <latiapelitos123@gmail.com>
2024-06-02 21:13:47 -0600

    Added Names for group

---
+++ src/const/team.ts
@@ -0,0 +1,27 @@
+interface Team {
+  name: string
+}
+
+export const team: Team[] = [
+  {
+    name: 'Erick Uriel Perez'
+  }, 
+  {
+    name: 'Mariel Guzman'
+  }, 
+  {
+    name: 'Dulce María Cruz'
+  }, 
+  {
+    name: 'Brian Fosado'
+  }, 
+  {
+    name: 'Ahtzire Yoselin Morales'
+  }, 
+  {
+    name: 'Alondra Juarez'
+  }, 
+  {
+    name: 'Jose Paul Martinez'
+  }
+]
